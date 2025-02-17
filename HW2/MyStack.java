9275c0f18f16c166df3464c90a0dfc1da5ea164c
jadddon <jadonyariv@gmail.com>
2025-02-17 17:11:15 -0600

    updates q2

---
+++ HW2/MyStack.java
@@ -0,0 +1,10 @@
+package HW2;
+
+import java.util.NoSuchElementException;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+public class MyStack<AnyType> implements MyStackInterface<AnyType> {
+    //initial size of stack
+    private static final int DEFAULT_CAPACITY = 5;
+}
